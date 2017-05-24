376ed0aa91ee873a8b07f3469d62241187d11618
tisu2tisu <catursura@gmail.com>
2017-05-24 20:24:55 +0700

    Added Security Anti-XSS vuln

--- func.php
+++ func.php
@@ -715,8 +715,8 @@
 				{
 					while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 					{
-						echo "<div class='comments-isi'>" . $row['nama'] . " &nbsp; " . $row['tanggal'] . "<br />";
-						echo $row['comments'] . "</div>";
+						echo "<div class='comments-isi'>" . htmlspecialchars($row['nama'], ENT_QUOTES, 'UTF-8') . "&nbsp;&nbsp;&nbsp;" . $row['tanggal'] . "<br /><br />";
+						echo htmlspecialchars($row['comments'], ENT_QUOTES, 'UTF-8') . "</div>";
 					}
 				}
 				else
@@ -725,8 +725,45 @@
 				}
 			}
 			
+			function xss_clean($data)
+				{
+				// Fix &entity\n;
+				$data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+				$data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+				$data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+				$data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+
+				// Remove any attribute starting with "on" or xmlns
+				$data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+
+				// Remove javascript: and vbscript: protocols
+				$data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+				$data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+				$data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+
+				// Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+				$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+				$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+				$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+
+				// Remove namespaced elements (we do not need them)
+				$data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+
+				do
+				{
+					// Remove really unwanted tags
+					$old_data = $data;
+					$data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+				}
+				while ($old_data !== $data);
+
+				// we are done...
+				return $data;
+				}
+			
 			public function prevent_spam_comments($ip, $article)
 			{
+			
 				$stmt = $this->db->prepare("SELECT article,ipaddress FROM comments WHERE ipaddress=:ip AND article=:article");
 				$stmt->bindParam(":ip", $ip);
 				$stmt->bindParam(":article", $article);
@@ -743,12 +780,15 @@
 				}
 			}
 			
+
 			public function create_comments($nama, $comments, $tanggal, $article, $ipaddress)
 			{
 				try
 				{	
 					if($this->prevent_spam_comments($ipaddress,$article))
 					{
+						$nama = $this->xss_clean($nama);
+						$comments = $this->xss_clean($comments);
 						$stmt = $this->db->prepare("INSERT INTO comments(nama,comments,tanggal,article,ipaddress) VALUES (:nama, :comments, :tanggal, :article, :ipaddress)");	
 						$stmt->bindParam(":nama", $nama);
 						$stmt->bindParam(":comments", $comments);
